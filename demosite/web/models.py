85f7cfb6122523cc24af99ba67673721bd411f13
Stephan Poetschner <stephan.poetschner@gmail.com>
2020-12-16 10:33:48 +0100

    + tmp

--- demosite/web/models.py
+++ demosite/web/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField('question', max_length=200)
     pub_date = models.DateTimeField('date published')
